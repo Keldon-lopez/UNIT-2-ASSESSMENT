ecf810d8ddfc2394ce24971a35e5f14c1b217a57
Keldon Lopez <keldon.lopz@gmail.com>
2023-02-05 15:19:23 -0700

    cart.js problem 1 completed

--- cart.js
+++ cart.js
@@ -7,7 +7,6 @@
     creating customer objects.  
 */
 
-
 //////////////////PROBLEM 1////////////////////
 /*  
     Below is a cart array that has food objects
@@ -19,24 +18,31 @@
 */
 
 const cart = [
-    {
-        name: 'pizza', 
-        price: 9.99
-    }, 
-    {
-        name: 'pasta', 
-        price: 8.99
-    }, 
-    {
-        name: 'salad', 
-        price: 7.99
-    }
-]
+  {
+    name: "pizza",
+    price: 9.99,
+  },
+  {
+    name: "pasta",
+    price: 8.99,
+  },
+  {
+    name: "salad",
+    price: 7.99,
+  },
+];
 
 //CODE HERE
+const getSum = (total, num) => {
+  if (typeof total === "number") {
+    return total + num.price;
+  } else {
+    return total.price + num.price;
+  }
+};
 
-// const summedPrice = cart.reduce(/* CALLBACK HERE */)
-
+const summedPrice = cart.reduce(getSum);
+console.log(summedPrice);
 
 //////////////////PROBLEM 2////////////////////
 /*  
@@ -54,8 +60,6 @@ const cart = [
 */
 
 //CODE HERE
-
-
 
 //////////////////PROBLEM 3////////////////////
 /*  
