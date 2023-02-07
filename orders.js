b6060f59870950502505d23731ca52d5f6e2cf1e
Keldon Lopez <keldon.lopz@gmail.com>
2023-02-07 16:37:55 -0700

    Finished Orders.js

--- orders.js
+++ orders.js
@@ -30,7 +30,17 @@
 */
 
 //CODE HERE
-
+class Ticket {
+    constructor(items, orderTime, customerId){
+        this.items = items;
+        this.orderTime = orderTime;
+        this.customerId = customerId;
+        this.status = 'queued';}
+    updateStatus(newStatus){
+        this.status = newStatus;
+        console.log(`The order for customer ${this.customerId} is now ${this.status}.`);
+    }
+}
 
 
 /*
@@ -45,7 +55,7 @@
 */
 
 //CODE HERE
-
+let firstTicket = new Ticket("pizza, bread, and soda", "7:03 PM", 575)
 
 /*
     Call the `updateStatus` method on
@@ -54,3 +64,4 @@
 */
 
 //CODE HERE
+firstTicket.updateStatus("Cooking");
